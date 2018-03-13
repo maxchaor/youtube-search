9483130fcf36984c31d576a76bdbe04a0447a060
cameronadams777 <cameronadams777@gmail.com>
2018-03-12 23:42:31 -0500

    first commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-layout-header>
+      <q-toolbar color="negative">
+        <q-toolbar-title>
+          YouTube Search
+          <div slot="subtitle">Running on Quasar v{{ $q.version }}</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-layout-header>
+
+    <q-layout-drawer
+      v-model="leftDrawerOpen"
+      content-class="bg-grey-2"
+    >
+      <q-list
+        no-border
+        link
+        inset-delimiter
+      >
+        <q-list-header>Results</q-list-header>
+      </q-list>
+    </q-layout-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'LayoutDefault'
+}
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-input float-label="Search:" :value="queryText"/>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      queryText: ''
+    }
+  }
+}
+</script>
